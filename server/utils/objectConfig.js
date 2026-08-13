f8e1fc1fd1ce957a3597df672d0277d10601cad8
shivahar <168921705+shivahar-git@users.noreply.github.com>
2026-08-13 13:15:57 +0530

    Add object configuration for various entities

--- server/utils/objectConfig.js
+++ server/utils/objectConfig.js
@@ -1,1 +1,94 @@
+const objectConfig = {
+  Account: {
+    fields: [
+      "Id",
+      "Name",
+      "Phone",
+      "Website",
+      "Industry",
+      "Type"
+    ],
+    createFields: [
+      "Name",
+      "Phone",
+      "Website",
+      "Industry",
+      "Type"
+    ]
+  },
 
+  Contact: {
+    fields: [
+      "Id",
+      "FirstName",
+      "LastName",
+      "Email",
+      "Phone",
+      "Title"
+    ],
+    createFields: [
+      "FirstName",
+      "LastName",
+      "Email",
+      "Phone",
+      "Title"
+    ]
+  },
+
+  Lead: {
+    fields: [
+      "Id",
+      "FirstName",
+      "LastName",
+      "Company",
+      "Email",
+      "Phone",
+      "Status"
+    ],
+    createFields: [
+      "FirstName",
+      "LastName",
+      "Company",
+      "Email",
+      "Phone",
+      "Status"
+    ]
+  },
+
+  Opportunity: {
+    fields: [
+      "Id",
+      "Name",
+      "StageName",
+      "CloseDate",
+      "Amount",
+      "Type"
+    ],
+    createFields: [
+      "Name",
+      "StageName",
+      "CloseDate",
+      "Amount",
+      "Type"
+    ]
+  },
+
+  Case: {
+    fields: [
+      "Id",
+      "CaseNumber",
+      "Subject",
+      "Status",
+      "Priority",
+      "Origin"
+    ],
+    createFields: [
+      "Subject",
+      "Status",
+      "Priority",
+      "Origin"
+    ]
+  }
+};
+
+module.exports = objectConfig;
