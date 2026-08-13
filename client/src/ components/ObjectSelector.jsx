652462e8d5c91e69fe8f23bf28150589ec31abb9
shivahar <168921705+shivahar-git@users.noreply.github.com>
2026-08-13 13:20:30 +0530

    Update ObjectSelector.jsx

--- client/src/ components/ObjectSelector.jsx
+++ client/src/ components/ObjectSelector.jsx
@@ -1,1 +1,33 @@
+function ObjectSelector({
+  selectedObject,
+  setSelectedObject
+}) {
 
+  const objects = [
+    "Account",
+    "Contact",
+    "Lead",
+    "Opportunity",
+    "Case"
+  ];
+
+  return (
+    <select
+      value={selectedObject}
+      onChange={(e) =>
+        setSelectedObject(e.target.value)
+      }
+    >
+      {objects.map((object) => (
+        <option
+          key={object}
+          value={object}
+        >
+          {object}
+        </option>
+      ))}
+    </select>
+  );
+}
+
+export default ObjectSelector;
