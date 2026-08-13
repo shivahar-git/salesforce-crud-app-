88d38db353d9de5e9128f6d92455ef2903412a2e
shivahar <168921705+shivahar-git@users.noreply.github.com>
2026-08-13 13:16:38 +0530

    Update records.js

--- server/routes/records.js
+++ server/routes/records.js
@@ -1,1 +1,13 @@
+const express = require("express");
+const axios = require("axios");
 
+const objectConfig = require("../utils/objectConfig");
+
+const router = express.Router();
+
+let authModule;
+
+router.use((req, res, next) => {
+  authModule = require("./auth");
+  next();
+});
