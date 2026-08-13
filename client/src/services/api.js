250bd8827b813c3157258f78b440220d77c55c8f
shivahar <168921705+shivahar-git@users.noreply.github.com>
2026-08-13 13:20:06 +0530

    Add API service functions for CRUD operations

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,1 +1,46 @@
+import axios from "axios";
 
+const API_URL = "http://localhost:5000";
+
+export const login = () => {
+  window.location.href =
+    `${API_URL}/auth/login`;
+};
+
+export const getSession = () =>
+  axios.get(`${API_URL}/auth/session`);
+
+export const getRecords = (
+  objectName,
+  offset
+) =>
+  axios.get(
+    `${API_URL}/api/records/${objectName}?offset=${offset}`
+  );
+
+export const createRecord = (
+  objectName,
+  data
+) =>
+  axios.post(
+    `${API_URL}/api/records/${objectName}`,
+    data
+  );
+
+export const updateRecord = (
+  objectName,
+  id,
+  data
+) =>
+  axios.patch(
+    `${API_URL}/api/records/${objectName}/${id}`,
+    data
+  );
+
+export const deleteRecord = (
+  objectName,
+  id
+) =>
+  axios.delete(
+    `${API_URL}/api/records/${objectName}/${id}`
+  );
